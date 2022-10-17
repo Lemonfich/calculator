32e594f7fe7c1bd97fd7b7b725367879ad4390de
Sergei Mahov <Lemonfich@gmail.com>
2022-10-17 21:15:34 +0700

    create view

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -9,6 +9,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UILabel!
+    
+    var numberForScreen = 0.0
+    
+    
+    
+    @IBAction func digits(_ sender: UIButton) {
+        textField.text =  textField.text! + String(sender.tag)
+        numberForScreen = Double(textField.text!)!
+        
+        
+    }
+    
+    
+    
+    @IBAction func buttons(_ sender: UIButton) {
+        if textField.text != "" {
+            if sender.tag == 11 {       //  /
+            }
+            else if  sender.tag == 12 {        //   X
+            }
+            else if sender.tag == 13 {         // -
+            }
+            else if sender.tag == 14 {        //   +
+            }
+            
+        }
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
